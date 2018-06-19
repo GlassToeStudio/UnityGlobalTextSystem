8ee8f80dd53c7a4b89deb754066f94fec19899f3
Glass Toe Studio <GlassToeStudio@gmail.com>
2018-06-19 17:15:15 -0500

    Add support for Unity versions below 2018.1

--- Assets/Editor/GlobalFontManager.cs
+++ Assets/Editor/GlobalFontManager.cs
@@ -11,14 +11,14 @@ namespace GlobalUITextFontUtility
     /// Class that will check every Text asset's Font
     /// to see if it is the GlobalFontData.
     /// </summary>
-    [InitializeOnLoad]
-    class GlobalFontManager
+    [System.Serializable, InitializeOnLoad]
+    static class GlobalFontManager
     {
         /// <summary>
         /// Get or Set the Global Font to be used for Every Text Object.
         /// </summary>
         public static FontData GlobalFontData { get; set; }
-        
+
         /// <summary>
         /// Only returns true of GlobalFontData is found and legit.
         /// </summary>
@@ -43,11 +43,11 @@ namespace GlobalUITextFontUtility
             // Load GlobalFontData
             GlobalFontData = GlobalFontUtils.LoadGlobalFont(globalFontName);
 
-            if(GlobalFontData == null )
+            if(GlobalFontData == null)
             {
                 Debug.LogWarning(
                     "<color=red>Global FontData not found! Please select one from Fonts/Select Global Font window!</color>."
-                    ); 
+                    );
 
                 return;
             }
@@ -59,14 +59,14 @@ namespace GlobalUITextFontUtility
 
                 return;
             }
-            
-                        
+
+
             if(GlobalFontData.name == "Arial")
             {
                 StopListening();
                 return;
             }
-            
+
             StartListening();
         }
 
@@ -75,7 +75,7 @@ namespace GlobalUITextFontUtility
         /// Get all Text assets in scene and get/set their font.
         /// </summary>
         private static void OnHierarchyChanged()
-        {            
+        {
             // Do not run while in play mode!
             if(Application.isEditor && !Application.isPlaying)
             {
@@ -83,7 +83,7 @@ namespace GlobalUITextFontUtility
                 {
                     return;
                 }
-                
+
                 Selection.activeGameObject.GetComponentInChildren<Text>(true).font = GlobalFontData.font;
 
             }
@@ -96,10 +96,18 @@ namespace GlobalUITextFontUtility
         {
             if(!IsInit)
             {
+                #if UNITY_2018_1_OR_NEWER
+
                 EditorApplication.hierarchyChanged += OnHierarchyChanged;
+
+                #else
+
+                EditorApplication.hierarchyWindowChanged += OnHierarchyChanged;
+
+                #endif
+
                 IsInit = true;
             }
-
         }
 
         /// <summary>
@@ -109,8 +117,16 @@ namespace GlobalUITextFontUtility
         {
             if(IsInit)
             {
+                #if UNITY_2018_1_OR_NEWER
+
                 EditorApplication.hierarchyChanged -= OnHierarchyChanged;
-                IsInit = false;  
+
+                #else
+
+                EditorApplication.hierarchyWindowChanged -= OnHierarchyChanged;
+
+                #endif
+                IsInit = false;
             }
         }
 
@@ -126,13 +142,13 @@ namespace GlobalUITextFontUtility
 
                 // Get all Text objects in the scene.
                 var allTextObjects = Resources.FindObjectsOfTypeAll(typeof(Text));
-                
+
                 // Loop each Text object and get/set its font.
                 // This will change the font for every Text object
                 // in the scene. Is this what we want?
                 foreach(Text t in allTextObjects)
                 {
-                    textCount ++;
+                    textCount++;
                     if(t.font != GlobalFontData.font)
                     {
                         fontChangedCount++;
@@ -143,12 +159,12 @@ namespace GlobalUITextFontUtility
                 Debug.Log(
                     string.Format(
                         "{0} Total Text objects found. {1} Total fonts changed!",
-                        textCount, 
+                        textCount,
                         fontChangedCount
                         ));
             }
         }
-        
+
         /// <summary>
         /// Checks that we have a created object (we could be deleting objects)
         /// And that the object, or its children, have Text.
@@ -161,45 +177,45 @@ namespace GlobalUITextFontUtility
                 return false;
             }
 
-            
+
             // If there is no Text object, abort.
             if(Selection.activeGameObject.GetComponentInChildren<Text>(true) == null)
             {
                 return false;
             }
-            
+
             return true;
         }
 
         /// <summary>
         /// Make sure we have set some Global Font Settings.
         /// </summary>
-        private static bool IsGlobalFontCriteriaMet() 
-        {        
+        private static bool IsGlobalFontCriteriaMet()
+        {
             // If a GlobalFontData is not set, abort.
             if(GlobalFontData == null)
             {
                 StopListening();
-                
+
                 Debug.LogError(
                     "<color=red>GlobalFontData is null, Have you set it?</color>"
                     );
 
                 return false;
             }
-            
+
             // If the font property of GlobalFontData is not set, abort.
             if(GlobalFontData.font == null)
             {
                 StopListening();
-                
+
                 Debug.LogError(
                     "<color=red>GlobalFont is null, did you delete the asset?</color>"
                     );
 
                 return false;
             }
-            
+
             return true;
         }
     }
